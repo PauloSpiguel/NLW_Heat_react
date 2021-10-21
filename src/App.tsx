1c74e1dfc4813f1e649fc123620cbee90d242f08
Paulo Spiguel <paulospil@hotmail.com>
2021-10-22 00:18:04 +0100

    feat(signin) add github signin

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import styles from './App.module.scss'
 import { LoginBox } from './components/LoginBox'
 import { MessageList } from './components/MessageList'
 
-
 function App() {
   return (
     <div className={styles.contentWrapper}>
--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,12 +1,53 @@
+import { useEffect } from "react";
 import { VscGithub } from "react-icons/vsc"
+import { api } from "../../services/api";
 
 import styles from "./styles.module.scss"
 
+interface GithubResponse {
+  token: string;
+  user: {
+    id: number;
+    login: string;
+    name: string;
+    avatar_url: string;
+  }
+}
+
 
 function LoginBox() {
+
+  const siginInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}`
+
+  async function siginIn(githubCode: string) {
+    const response = await api.post<GithubResponse>("authenticate", {
+      code: githubCode
+    })
+
+    const { token, user } = response.data
+
+    localStorage.setItem("@doWhile.token", token)
+
+    console.log(user);
+
+  }
+
+  useEffect(() => {
+
+    const url = window.location.href
+    const [urlWithoutCode, githubCode] = url.split("?code=")
+
+    console.log({ urlWithoutCode, githubCode })
+
+    window.history.pushState({}, "", urlWithoutCode)
+
+    siginIn(githubCode)
+
+  }, [])
+
   return <div className={styles.loginWrapper}>
     <strong>Entre e compartinhe sua mensagem</strong>
-    <a href="#" className={styles.signInWithGithub}>
+    <a href={siginInUrl} className={styles.signInWithGithub}>
       <VscGithub size={24} />  Entrar com Github
     </a>
   </div>;
